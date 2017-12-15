3f31006634b645d0d69f43495ead69315161d198
Sanket Nandakumar Pednekar <sp2363@afsconnect2.njit.edu>
2017-12-15 15:27:52 -0500

    adding active record files

---
+++ collection/todos.php
@@ -0,0 +1,11 @@
+<?php
+namespace classes\collection;
+use classes\collection\collection;
+final class todos extends collection {
+	protected static $modelName = 'todo';
+    	public static function tableName1(){
+		$tableName='todos';
+	      	return $tableName;
+	}
+}
+?>
---
+++ model/todo.php
@@ -0,0 +1,19 @@
+<?php
+namespace classes\model;
+use classes\model\model;
+final class todo extends model {
+	protected static $modelName = 'todo';
+      	public $id;
+      	public $owneremail;
+	public $ownerid;
+	public $createddate;
+	public $duedate;
+	public $message;
+	public $isdone;
+						  
+	public static function tableName(){
+		$tableName='todos';
+		return $tableName;
+	}
+}
+?>
